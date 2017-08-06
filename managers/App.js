7f18f5d4f2d253d0cb47780510a9874e3d13283e
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-06 14:30:22 -0700

    working user auth

--- managers/App.js
+++ managers/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './src/reducers';
 import firebase from 'firebase';
 import LoginForm from './src/components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 export default class App extends Component {
   componentWillMount() {
@@ -21,8 +22,9 @@ export default class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View style={styles.container}>
           <LoginForm />
         </View>
--- managers/src/reducers/AuthReducer.js
+++ managers/src/reducers/AuthReducer.js
@@ -1,7 +1,32 @@
-const INITIAL_STATE = { email: '' };
+import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER} from '../actions/types';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
+
   switch (action.type) {
+    case EMAIL_CHANGED:
+      state.email = action.payload;
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      state.password = action.payload;
+      return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: ''};
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        ...INITIAL_STATE,
+        user: action.payload
+      };
+    case LOGIN_USER_FAIL:
+      return { ...state, error: 'Authentication failed', password: '', loading: false };
     default:
       return state;
   }
